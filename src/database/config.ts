122ab80e139c6d1f45b4b0b679ace241b340909d
caldayham <caldayham@gmail.com>
2023-05-01 21:54:20 -0700

    correct env stuff

--- src/database/config.ts
+++ src/database/config.ts
@@ -4,8 +4,8 @@ dotenv.config();
 
 export default {
   aws: {
-    region: process.env.AWS_DEFAULT_REGION,
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    region: process.env.AWS_DYNAMODB_REGION,
+    accessKeyId: process.env.AWS_DYNAMODB_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_DYNAMODB_SECRET_ACCESS_KEY,
   },
 };
