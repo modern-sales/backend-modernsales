b71e574e3dd56f53f88a83eeb17b88bbb14d1a72
caldayham <caldayham@gmail.com>
2023-05-05 15:53:01 -0700

    deploy test

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello, this is the Modern Sales backend, hope it works, v7!';
+    return 'Hello, this is the Modern Sales backend, hope it works, v9!';
   }
 }
