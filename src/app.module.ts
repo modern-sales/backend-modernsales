332e6271ba65a2e7217ed0b3815cb80134c75987
caldayham <caldayham@gmail.com>
2023-05-13 20:21:33 -0700

    removed old auth module and associated files for simplifiation of deployment

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,19 +4,19 @@ import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config'; // Import ConfigModule
 import { UsersModule } from '@modules/users/users.module';
 import { CoursesModule } from '@modules/courses/courses.module';
-import { AuthModule } from '@modules/auth/auth.module';
-
 import { EmailModule } from '@services/aws_ses/email.module';
 import { StripeModule } from '@services/stripe/stripe.module';
+import { AuthModule } from '@modules/auth/auth.module';
+
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
     UsersModule,
     CoursesModule,
-    AuthModule,
     EmailModule,
-    StripeModule
+    StripeModule,
+    AuthModule
   ],
   controllers: [AppController],
   providers: [AppService],
--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -1,25 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AuthService } from './auth.service';
-import { AuthController } from './auth.controller';
-import { UsersModule } from '../users/users.module'; // Import UsersModule here
-import { JwtStrategy } from './middleware/jwt.strategy';
-import { PassportModule } from '@nestjs/passport';
-import { JwtModule } from '@nestjs/jwt';
-import { jwtConstants } from '@config/app.config';
-import { EmailModule } from '@services/aws_ses/email.module';
 
-@Module({
-  imports: [
-    UsersModule, // Add UsersModule to the imports array
-    EmailModule,
-    PassportModule.register({ defaultStrategy: 'jwt' }),
-    JwtModule.register({
-      secret: jwtConstants.secret,
-      signOptions: { expiresIn: '60m' },
-    }),
-  ],
-  controllers: [AuthController],
-  providers: [AuthService, JwtStrategy],
-  exports: [AuthService]
-})
+@Module({})
 export class AuthModule {}
